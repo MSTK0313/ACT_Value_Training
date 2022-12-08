a772c43ded9965746206e7f67f6e062a87932e70
Masataka Sato <mas.0313.liberty@gmail.com>
2022-12-08 11:08:57 +0900

    create login file

---
+++ Login_View.swift
@@ -0,0 +1,20 @@
+//
+//  Login_View.swift
+//  JACK
+//
+//  Created by Sato Masataka on 2022/12/08.
+//
+
+import SwiftUI
+
+struct Login_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Login_View()
+    }
+}
